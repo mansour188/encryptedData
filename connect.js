af185fb777712a4f4396d3e0aac31c1c08cb7e96
mansour <mansourbenahmed89@gmail.com>
2023-11-22 20:03:02 +0100

    map port of container to host

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ const {createUserIfNotExists}=require('./routers/UserRouter')
 
 
 
-mongoose.connect(`mongodb+srv://new:Z4jzQNKyCNTLKRpd@cluster0.aot4fyq.mongodb.net/secuirty?retryWrites=true&w=majority`).then(()=>{
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.aot4fyq.mongodb.net/secuirty?retryWrites=true&w=majority`).then(()=>{
     console.log("mongoose connected ...")
     createUserIfNotExists()
 }).catch((err)=>{
